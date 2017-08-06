7060995ae90a3b3a758f9e8c0f37609b1760ca77
Eric P. Katz <ericpkatz@gmail.com>
2017-08-06 13:00:59 -0400

    commit

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
 app.set('view engine', 'html');
 app.engine('html', swig.renderFile);
 
+app.use((req, res, next)=> {
+  res.locals.url = req.url;
+  next();
+});
+
 app.get('/', (req, res, next)=> {
   res.render('index');
 });
