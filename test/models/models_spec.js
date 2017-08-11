04f109d9ae0bf854c5438b6cd4534e5d4eb7baf8
Eric P. Katz <ericpkatz@gmail.com>
2017-08-10 21:16:37 -0400

    some more tests

--- test/models/models_spec.js
+++ test/models/models_spec.js
@@ -38,4 +38,25 @@ describe('User', ()=> {
   it('moe has two awards', ()=> {
     expect(moe.awards.length).to.equal(2);
   });
+
+  describe('remove award', ()=> {
+    it('removes mentees if less than 2 awards', ()=> {
+      return User.removeAward(moe.id, moe.awards[0].id)
+        .then(()=> {
+          return User.findAll({ where: { mentorId: moe.id } });
+        })
+        .then( mentees => expect(mentees.length).to.equal(0));
+
+    });
+  });
+
+  describe('findUsersViewModel', ()=> {
+    it('returns users and available mentors', ()=> {
+      return User.findUsersViewModel()
+        .then( model => {
+          expect(model.users.length).to.equal(4);
+          expect(model.availableMentorsMap).to.be.ok;
+        });
+    });
+  });
 });
